e0883d00b3de00c6aea86ef54597fd5324b72695
eszw10 <erycszw10@gmail.com>
2022-11-09 15:56:04 +0700

    fix style and add some javascript

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 let name = prompt('Halo! Siapa nama anda? ')
 alert(`Selamat Datang ${name}!`);
+let navbar = document.getElementById("nav");
+let sections = document.querySelectorAll('section')
+window.onscroll = () => {
+    "use strict";
+    if(document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
+        navbar.classList.add("nav-scrolled");
+    } else {
+        navbar.classList.remove("nav-scrolled");
+    }
+
+    sections.forEach(section=> {
+        let current = ''
+        let sectionTop = section.offsetTop;
+        if(scrollY >= sectionTop-500) {
+            current = section.getAttribute('id');
+            if(current == 'dashboard') {
+                document.getElementById('home').classList.add('active')
+                document.getElementById('a-about').classList.remove('active')
+                document.getElementById('a-project').classList.remove('active')
+                document.getElementById('a-contacts').classList.remove('active')
+            } else if(current == 'about') {
+                document.getElementById('a-about').classList.add('active')
+                document.getElementById('home').classList.remove('active')
+                document.getElementById('a-project').classList.remove('active')
+                document.getElementById('a-contacts').classList.remove('active')
+            } else if(current == 'project') {
+                document.getElementById('a-project').classList.add('active')
+                document.getElementById('a-about').classList.remove('active')
+                document.getElementById('home').classList.remove('active')
+                document.getElementById('a-contacts').classList.remove('active')
+            } else if(current == 'contacts') {
+                document.getElementById('a-contacts').classList.add('active')
+                document.getElementById('a-about').classList.remove('active')
+                document.getElementById('home').classList.remove('active')
+                document.getElementById('a-project').classList.remove('active')
+            }
+        }
+    })
+
+};
